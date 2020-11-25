37aa80c9a346c3bb2c9b268fffab64ece3cf5f50
KaranKakouei <38806763+KaranKakouei@users.noreply.github.com>
2020-11-25 14:16:43 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -36,26 +36,44 @@ dashboardPage(     skin = "blue", # theme = shinytheme("cosmo"),
           column(width = 1),
           column(width = 10, 
           p("About me", style = "padding-top: 10px; font-size: 30px; font-weight:bold;"),
-          p("For the last seven years, I have been working at the Leibniz-Institute of Freshwater Ecology and Inland Fisheries whereI have been 
-          responsible for analysis of big data using machine/deep learning tools to assess predictive power of featureparameters or influencing 
-          factors and define the potential scopes with implications for environmental management.I have also been responsible for contributing 
-          to talking about data and thriving on explaining the meaning of databy providing easily interpretable outcomes (e.g., interactive dashboards, 
-          reports and deliverables) for either my fellowcolleagues or scientists, managers, policymakers and non-technical stakeholders. During my 
-          education and scientific life,I have developed a strong background in programming that has served me well in my pursuit to advance the 
-          interestsof my employers. I am highly experienced in the management, cleaning and analysis of large data sets. With more thanseven years 
-          of R and two years of Python programming, and two years of shell scripting experience, I see myself as adata scientist who extremely enjoys 
-          preparing complex scripts to perform analytical modelling of big data, and effectivelyvisualizing multi-dimensional data.", style= "text-align: justify; font-size: 18px;"),
-          p("I have the initiative, enthusiasm and willingness to learn new skills and keep abreast of and promote new developmentsin the appropriate 
-          professional field. Thanks to my migration background and my experience of studying and workingin different countries (Iran, The Netherlands 
+          p("I am a motivated, team-work oriented, and responsible Data Scientist and Water Manager with seven years of practical experience in Machine 
+          Learning and predictive modeling of complex data, which includes handling the whole pipeline from preprocessing of data tothe visualization of results. 
+          I am highly proficient in SQL and in programming languages such as R and Python, and have an open mind-set and a commitment to continuous learning and 
+          professional development.", style= "text-align: justify; font-size: 18px;"),
+      
+          p("For the last seven years, I have been working at the Leibniz-Institute of Freshwater Ecology and Inland Fisheries where I have been responsible 
+          for analysis of big data using machine and deep learning tools to assess predictive power of featureparameters or influencing factors and define the 
+          potential future scopes with implications for environmental management. I have also been responsible for contributing to talking about data and 
+          thriving on explaining the meaning of data by providing easily interpretable outcomes (e.g., interactive dashboards, reports and deliverables) for 
+          either my fellow colleagues or managers and non-technical stakeholders.", style= "text-align: justify; font-size: 18px;"),
+      
+          p("I have the initiative, enthusiasm and willingness to learn new skills and keep abreast of and promote new developments in the appropriate 
+          professional field. Thanks to my migration background and my experience of studying and working in different countries (Iran, The Netherlands 
           and Germany) I have become very flexible and able to adapt to differentworking environments while communicating in different languages, which 
           I really enjoy.", style= "text-align: justify; font-size: 18px;"),
+      
+          p("Here I work on a variety of kaggle competitions and would like to distribute my competitive solutions, so people might come up with potential 
+          ways of solving real-life tasks. The good news is that I anounce my solutions in both R and python environments. Yaaaay!", 
+          style= "text-align: justify; font-size: 18px;"),
+
           p("Data", style = "padding-top: 10px; font-size: 30px; font-weight:bold;"),
+              
           p("Appliances energy prediction", style = "font-size: 20px; ; font-style: italic; font-weight:bold;"),
-          p("Appliances energy use is recorded at 10-minute frequencies for roughly 4.5 months. The house temperature and humidity conditions were monitored with a ZigBee wireless sensor network. 
-          Each wireless node transmitted the temperature and humidity conditions around 3.3 min. Then, the wireless data was averaged for 10 minutes periods. 
-          The energy data was logged every 10 minutes with m-bus energy meters. Weather from the nearest airport weather station (Chievres Airport, Belgium) 
-          was downloaded from a public data set from Reliable Prognosis (rp5.ru), and merged together with the experimental data sets using the date and time column. 
-          Two random variables have been included in the data set for testing the regression models and to filter out non predictive attributes (parameters). You can download the data ", tags$a(href="https://www.kaggle.com/loveall/appliances-energy-prediction", "here on Kaggle."), style= "text-align: justify; font-size: 18px;"),
+              
+          p("The first project I am working on is the "Appliances Energy Use AEU". The AEU includes high resolution data that is recorded at 10-minute 
+          frequencies for roughly 4.5 months. The recorded feature parameters are temperature (11 parameters), relative humidity (10 parameters), lights, 
+          wind speed, precipitation, and visibility. According to the project descriptions, the house temperature and humidity conditions were monitored 
+          with a ZigBee wireless sensor network. Each wireless node transmitted the temperature and humidity conditions around 3.3 min. Then, the wireless 
+          data was averaged for 10 minutes periods. The energy data was logged every 10 minutes with m-bus energy meters. Weather from the nearest airport 
+          weather station (Chievres Airport, Belgium) was downloaded from a public data set from Reliable Prognosis (rp5.ru), and merged together with the 
+          experimental data sets using the date and time column. Two random variables have been included in the data set for testing the regression models 
+          and to filter out non predictive attributes (parameters).  You can download the data ", tags$a(href="https://www.kaggle.com/loveall/appliances-energy-prediction", "here on Kaggle."), 
+          style= "text-align: justify; font-size: 18px;"),
+
+          p("To follow the whole pipeline from preprocessing of data to the visualization of the results, I prepared an interractive shiny web application 
+          which includes all the scripts and visualized outcomes. I will update this read me file as well as the dashboard once I have solutions for further 
+          projects. Enjoy scrolling through the interractive dashboard!"), style= "text-align: justify; font-size: 18px;")
+
           p("R and Python scripts", style = "padding-top: 10px; font-size: 30px; font-weight:bold;"),
           p("If you are interested in the codes presented in this shiny app, please download thescripts under my ", tags$a(href="https://github.com/KaranKakouei/KaranHub/", "GitHub repository."), style= "text-align: justify; font-size: 18px;")
           )), column(width = 2)
