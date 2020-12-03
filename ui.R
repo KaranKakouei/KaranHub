8878807b99ba3dddec0adbff3f9233a76b73b34d
Karan Kakouei <38806763+KaranKakouei@users.noreply.github.com>
2020-12-03 12:07:18 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -58,7 +58,7 @@ ui <-
 
             p("Appliances energy prediction", style = "font-size: 20px; ; font-style: italic; font-weight:bold;"),
 
-            p("The first project I am working on is the "Appliances Energy Use AEU". The AEU includes high resolution data that is recorded at 10-minute 
+            p("The first project I am working on is the 'Appliances Energy Use AEU'. The AEU includes high resolution data that is recorded at 10-minute 
             frequencies for roughly 4.5 months. The recorded feature parameters are temperature (11 parameters), relative humidity (10 parameters), lights, 
             wind speed, precipitation, and visibility. According to the project descriptions, the house temperature and humidity conditions were monitored 
             with a ZigBee wireless sensor network. Each wireless node transmitted the temperature and humidity conditions around 3.3 min. Then, the wireless 
